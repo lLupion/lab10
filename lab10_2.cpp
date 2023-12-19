172c748a4552de259978b15e8053748fc49f1068
Wichuda <Vichudarodna.0569@gmail.com>
2023-12-19 21:38:19 +0700

    lab10_2 done

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,15 +6,14 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	dest << "-------------------- BOOM ---------------------";
-	
+	dest << "-------------------- BOOM ---------------------\n";
 	string textline;
-	bool havetext;
-	//havetext = getline(source, textline);
-
+	while(getline(source,textline)){
+		cout << textline << "\n";
+	}
 	dest << "-------------------- HA!! ---------------------";
 
     source.close();
